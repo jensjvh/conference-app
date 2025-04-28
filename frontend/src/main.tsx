cfbc6f759bb119964dbcba23ef65c23e72478e98
jensjvh <43621451+jensjvh@users.noreply.github.com>
2025-04-28 13:18:39 +0300

    Refactor app to use routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import './index.css';
+import App from './app/App.tsx';
+import ConferenceMap from './app/routes/map.tsx'
+import UsefulLinks from './app/routes/links.tsx'
+import Schedule from './app/routes/schedule.tsx'
+import Layout from './components/Layout.tsx';
+import { ThemeProvider } from "@mui/material/styles";
+import { createTheme } from "@mui/material/styles";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#00629B",
+      },
+      secondary: {
+        main: "#ffffff",
+      },
+    },
+  });
+
+const root = document.getElementById("root")!;
+
+ReactDOM.createRoot(root).render(
+    <StrictMode>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<App />} />
+              <Route path="/map" element={<ConferenceMap floor={1} />} />
+              <Route path="/links" element={<UsefulLinks />} />
+              <Route path="/schedule" element={<Schedule />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </StrictMode>
+  );
