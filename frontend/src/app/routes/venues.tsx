e291e2a8c02fda44b92437d4687d0cd005729f64
jensjvh <43621451+jensjvh@users.noreply.github.com>
2025-06-19 19:25:07 +0300

    Add correct paths

--- frontend/src/app/routes/venues.tsx
+++ frontend/src/app/routes/venues.tsx
@@ -5,19 +5,19 @@ const locations = [
     name: "University of Helsinki, Main Building, Fabianinkatu 33",
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1984.6787311175224!2d24.947933177931343!3d60.169497243487534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920bce5e8c3d29%3A0xe5227821fc988c1e!2sUniversity%20of%20Helsinki%2C%20Main%20Building!5e0!3m2!1sen!2sfi!4v1750147676368!5m2!1sen!2sfi",
-    image: "/img/University.jpg",
+    image: import.meta.env.BASE_URL + "img/University.jpg",
   },
   {
     name: "Helsinki City Hall, Banquet Hall, Pohjoisesplanadi 11–13",
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1984.765652735059!2d24.950334977931306!3d60.16805834360357!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920bcf052f978b%3A0xda23a21022a81997!2sHelsinki%20City%20Hall!5e0!3m2!1sen!2sfi!4v1750148186485!5m2!1sen!2sfi",
-    image: "/img/CityHall.jpg",
+    image: import.meta.env.BASE_URL + "img/CityHall.jpg",
   },
   {
     name: "The Banquet Hall, Ravintola Töölö, Runeberginkatu 14–16",
     mapUrl:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7938.188768427484!2d24.904080987158203!3d60.17167470000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46920bc6b0a494eb%3A0xe61913c762dca278!2zUmF2aW50b2xhIFTDtsO2bMO2!5e0!3m2!1sen!2sfi!4v1750148283683!5m2!1sen!2sfi",
-    image: "/img/Restaurant.jpg",
+    image: import.meta.env.BASE_URL + "img/Restaurant.jpg",
   }
 ];
 
